e62c1a5147a33eb396fd318b92c3d9425ae07543
Alex Rock Ancelet <pierstoval@gmail.com>
2024-12-19 16:57:18 +0100

    Move db update check out of StandardIncludes to its own listener+controller

--- src/Glpi/Config/LegacyConfigurators/StandardIncludes.php
+++ src/Glpi/Config/LegacyConfigurators/StandardIncludes.php
@@ -34,19 +34,11 @@
 
 namespace Glpi\Config\LegacyConfigurators;
 
-use Config;
 use Glpi\Application\View\TemplateRenderer;
 use Glpi\Config\ConfigProviderHasRequestTrait;
 use Glpi\Config\ConfigProviderWithRequestInterface;
 use Glpi\Config\LegacyConfigProviderInterface;
 use Glpi\Http\RequestPoliciesTrait;
-use Glpi\System\Requirement\DatabaseTablesEngine;
-use Glpi\System\RequirementsManager;
-use Glpi\Toolbox\VersionParser;
-use Html;
-use Session;
-use Toolbox;
-use Update;
 
 final class StandardIncludes implements LegacyConfigProviderInterface, ConfigProviderWithRequestInterface
 {
@@ -62,10 +54,10 @@ final class StandardIncludes implements LegacyConfigProviderInterface, ConfigPro
 
         // Check maintenance mode
         if (
-            !$this->isFrontEndAssetEndpoint($this->getRequest())
-            && !$this->isSymfonyProfilerEndpoint($this->getRequest())
-            && isset($CFG_GLPI["maintenance_mode"])
+            isset($CFG_GLPI["maintenance_mode"])
             && $CFG_GLPI["maintenance_mode"]
+            && !$this->isFrontEndAssetEndpoint($this->getRequest())
+            && !$this->isSymfonyProfilerEndpoint($this->getRequest())
         ) {
             if (isset($_GET['skipMaintenance']) && $_GET['skipMaintenance']) {
                 $_SESSION["glpiskipMaintenance"] = 1;
@@ -78,78 +70,6 @@ final class StandardIncludes implements LegacyConfigProviderInterface, ConfigPro
                 ]);
                 exit();
             }
-        }
-
-        // Check version
-        if ($this->shouldCheckDbStatus($this->getRequest()) && !defined('SKIP_UPDATES') && !Update::isDbUpToDate()) {
-            // Prevent debug bar to be displayed when an admin user was connected with debug mode when codebase was updated.
-            $debug_mode = $_SESSION['glpi_use_mode'];
-            Toolbox::setDebugMode(Session::NORMAL_MODE);
-
-            /** @var \DBmysql $DB */
-            global $DB;
-
-            $requirements = (new RequirementsManager())->getCoreRequirementList($DB);
-            $requirements->add(new DatabaseTablesEngine($DB));
-
-            $twig_params = [
-                'core_requirements' => $requirements,
-                'try_again'         => __('Try again'),
-                'update_needed'     => __('The GLPI codebase has been updated. The update of the GLPI database is necessary.'),
-                'upgrade'           => _sx('button', 'Upgrade'),
-                'outdated_files'    => __('You are trying to use GLPI with outdated files compared to the version of the database. Please install the correct GLPI files corresponding to the version of your database.'),
-                'stable_release'    => VersionParser::isStableRelease(GLPI_VERSION),
-                'agree_unstable'    => Config::agreeUnstableMessage(VersionParser::isDevVersion(GLPI_VERSION)),
-                'outdated'          => version_compare(
-                    VersionParser::getNormalizedVersion($CFG_GLPI['version'] ?? '0.0.0-dev'),
-                    VersionParser::getNormalizedVersion(GLPI_VERSION),
-                    '>'
-                )
-            ];
-
-            Html::nullHeader(__('Update needed'), $CFG_GLPI["root_doc"]);
-            // language=Twig
-            echo TemplateRenderer::getInstance()->renderFromStringTemplate(<<<TWIG
-            <div class="container-fluid mb-4">
-                <div class="row justify-content-evenly">
-                    <div class="col-12 col-xxl-6">
-                        <div class="card text-center mb-4">
-                            {% include 'install/blocks/requirements_table.html.twig' with {'requirements': core_requirements} %}
-                            {% if core_requirements.hasMissingMandatoryRequirements() or core_requirements.hasMissingOptionalRequirements() %}
-                                <form action="{{ path('index.php') }}" method="post">
-                                    <button type="submit" class="btn btn-primary">
-                                        <i class="fas fa-redo"></i>{{ try_again }}
-                                    </button>
-                                </form>
-                            {% endif %}
-                            {% if not core_requirements.hasMissingMandatoryRequirements() %}
-                                {% if not outdated %}
-                                    <form method="post" action="{{ path('install/update.php') }}">
-                                        <input type="hidden" name="_glpi_csrf_token" value="{{ csrf_token() }}">
-                                        {% if not stable_release %}
-                                            {{ agree_unstable|raw }}
-                                        {% endif %}
-                                        <p class="mt-2 mb-n2 alert alert-important alert-warning">
-                                            {{ update_needed }}
-                                        </p>
-                                        <button type="submit" name="from_update" class="btn btn-primary">
-                                            <i class="fas fa-check"></i>{{ upgrade }}
-                                        </button>
-                                    </form>
-                                {% else %}
-                                    <p class="mt-2 mb-n2 alert alert-important alert-warning">
-                                        {{ outdated_files }}
-                                    </p>
-                                {% endif %}
-                            {% endif %}
-                        </div>
-                    </div>
-                </div>
-            </div>
-TWIG, $twig_params);
-            Html::nullFooter();
-            $_SESSION['glpi_use_mode'] = $debug_mode;
-            exit();
         }
     }
 }
